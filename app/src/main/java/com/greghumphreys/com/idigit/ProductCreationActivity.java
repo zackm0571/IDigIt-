61b9a75d08e24e522714e9595cc3580a0e4c83a2
Zack Matthews <zack@zackmatthews.com>
2015-04-21 09:24:08 -0400

    Added Product Creation

---
+++ app/src/main/java/com/greghumphreys/com/idigit/ProductCreationActivity.java
@@ -0,0 +1,18 @@
+package com.greghumphreys.com.idigit;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ProductCreationActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_creation_layout);
+    }
+
+
+}
